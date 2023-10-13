f3e6c0bdf127cc178c92ed9f57c2ca6653b106be
CookieMog <webdevapoillot@gmail.com>
2023-10-13 13:48:47 -0400

    Tweak after hosting ( for screen width issues)

--- views/components/head.php
+++ views/components/head.php
@@ -6,5 +6,5 @@
     <script src="https://kit.fontawesome.com/50ab74dd38.js" crossorigin="anonymous"></script>
     <script src="https://cdn.tiny.cloud/1/0ki8kh44dpr0iupw97fjo8e0x3vrccw5i082axf6oco1elwv/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tinymce/tinymce-webcomponent@1/dist/tinymce-webcomponent.min.js"></script>
-    <title>Document</title>
+    <title>Blog de Jean Forteroche</title>
 </head>
